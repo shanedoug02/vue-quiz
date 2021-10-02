a6b88e798bccd104d529ac94dc55ebdbd4fd7e8b
Shane Dougherty <shane@caughtupgoods.com>
2021-10-02 10:25:48 -0600

    Added isCorrect and isSelected to Data

--- src/components/QuizChild.vue
+++ src/components/QuizChild.vue
@@ -1,19 +1,49 @@
 <template>
-	<div>
-		<div>{{ quizData }}</div>
+	<div class="quiz-child-container">
+		<div v-for="(item, idx) of quizData" :key="item">
+			<div v-if="idx === currentQuestion">
+				<h1>{{ item.question }}</h1>
+				<div class="answers-container" v-for="(answer, index) of item.answersArr" :key="answer">
+					<div @click="triggerSelected(answer)" class="check-box" :id="index" :class="{ selected: answer.isSelected }"></div>
+					{{ answer.option }}
+				</div>
+			</div>
+		</div>
+		<button @click="saveSelected">next</button>
 	</div>
 </template>
 
 <script>
 export default {
 	name: 'QuizChild',
-	props: ['quizData'],
+	props: ['quizData', 'currentQuestion'],
+	methods: {
+		triggerSelected(emittedData) {
+			this.$emit('emittedSelection', emittedData);
+		},
+		saveSelected() {
+			this.$emit('evaluate');
+		},
+	},
 };
 </script>
 <style>
-* {
-	margin: 0;
-	padding: 0;
-	box-sizing: border-box;
+.quiz-child-container {
+	margin: 1rem;
+}
+
+.answers-container {
+	display: flex;
+	align-items: center;
+}
+
+.check-box {
+	height: 10px;
+	width: 10px;
+	outline: 1px solid rgb(209, 206, 206);
+	margin-right: 10px;
+}
+.selected {
+	background: cyan;
 }
 </style>
--- src/components/QuizParent.vue
+++ src/components/QuizParent.vue
@@ -1,5 +1,5 @@
 <template>
-	<QuizChild :quizData="quizData" />
+	<QuizChild :quizData="quizData" :currentQuestion="currentQuestion" @emittedSelection="setSelected" @evaluate="see" />
 </template>
 
 <script>
@@ -12,31 +12,64 @@ export default {
 			quizData: [
 				{
 					question: 'Which of the following is not a real eCommerce platform?',
-					answersArr: ['Shopify', 'WooCommerce', 'ShopCommerce', 'BigCommerce'],
-					correctAnswerIndex: 2,
+					answersArr: [
+						{ option: 'Shopify', isCorrect: false, isSelected: false },
+						{ option: 'WooCommerce', isCorrect: false, isSelected: false },
+						{ option: 'ShopCommerce', isCorrect: true, isSelected: false },
+						{ option: 'BigCommerce', isCorrect: false, isSelected: false },
+					],
 				},
 				{
 					question: 'If Shopify is so good, why are Shopify developers necessary?',
 					answersArr: [
-						'To save time on things like store setups and migrations',
-						'To extend the limited design options and functionalities of themes with custom code',
-						'To provide support with a deep understanding of how the platform works and what its limitations are',
-						'All the above',
+						{ option: 'To save time on things like store setups and migrations', isCorrect: false, isSelected: false },
+						{ option: 'To extend the limited design options and functionalities of themes with custom code', isCorrect: false, isSelected: false },
+						{
+							option: 'To provide support with a deep understanding of how the platform works and what its limitations are',
+							isCorrect: false,
+							isSelected: false,
+						},
+						{ option: 'All the above', isCorrect: true, isSelected: false },
 					],
-					correctAnswerIndex: 3,
 				},
 				{
 					question: ' Which of the following is true about Shopify developers?',
 					answersArr: [
-						'They are paid extremely well',
-						'There is a high demand for them',
-						'They need to know web development, the platform itself, and the liquid template language',
-						'All the above',
+						{ option: 'They are paid extremely well', isCorrect: false, isSelected: false },
+						{ option: 'There is a high demand for them', isCorrect: false, isSelected: false },
+						{
+							option: 'They need to know web development, the platform itself, and the liquid template language',
+							isCorrect: false,
+							isSelected: false,
+						},
+						{ option: 'All the above', isCorrect: true, isSelected: false },
 					],
-					correctAnswerIndex: 3,
 				},
 			],
+			currentQuestion: 0,
+			currentSelection: [],
+			finalSelections: [],
+			results: [],
 		};
+	},
+	methods: {
+		setSelected(payload) {
+			for (let item of this.quizData) {
+				for (let ans of item.answersArr) {
+					ans.isSelected = false;
+				}
+			}
+			payload.isSelected = true;
+			if (payload.isSelected) {
+				this.currentSelection.unshift(payload.option);
+				console.log(this.currentSelection);
+			}
+		},
+		see() {
+			this.finalSelections.push(this.currentSelection[0]);
+			this.currentQuestion++;
+			console.log('finals', this.finalSelections);
+		},
 	},
 };
 </script>
